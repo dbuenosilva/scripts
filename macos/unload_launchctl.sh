8a402f600298b8727af7d94320c0546168667ad2
Diego Bueno <diego@gwaya.com>
2020-05-09 05:12:23 -0300

    Add files via upload

---
+++ macos/unload_launchctl.sh
@@ -0,0 +1,20 @@
+sudo launchctl unload com.fortinet.forticlient.appfw.plist  
+sudo stop unload com.fortinet.forticlient.appfw.plist  
+
+sudo launchctl unload com.fortinet.forticlient.epctrl.plist   
+sudo stop unload com.fortinet.forticlient.epctrl.plist   
+
+sudo launchctl unload com.fortinet.forticlient.servctl.plist   
+sudo stop unload com.fortinet.forticlient.servctl.plist  
+
+sudo launchctl unload com.fortinet.forticlient.vpn.plist  
+sudo stop unload com.fortinet.forticlient.vpn.plist  
+
+sudo launchctl unload com.github.IngmarStein.Monolingual.Helper.plist  
+sudo stop unload com.github.IngmarStein.Monolingual.Helper.plist  
+
+sudo launchctl unload com.teamviewer.teamviewer_service.plist     
+sudo stop unload com.teamviewer.teamviewer_service.plist     
+
+sudo launchctl unload net.tunnelblick.tunnelblick.tunnelblickd.plist  
+sudo stop unload net.tunnelblick.tunnelblick.tunnelblickd.plist  
